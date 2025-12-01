7744d01f5487db32a6e2b21fd378b2523b8dd38e
filipxg <filip.m.gunnarsson@gmail.com>
2025-12-01 10:20:00 +0100

    Create folders for days 2-12 and update README

--- README.md
+++ README.md
@@ -15,6 +15,17 @@ Inside each folder, you'll typically find:
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---: |
 | [Day 1](day1/) | ⭐ | ⭐ |
+| [Day 2](day2/) | | |
+| [Day 3](day3/) | | |
+| [Day 4](day4/) | | |
+| [Day 5](day5/) | | |
+| [Day 6](day6/) | | |
+| [Day 7](day7/) | | |
+| [Day 8](day8/) | | |
+| [Day 9](day9/) | | |
+| [Day 10](day10/) | | |
+| [Day 11](day11/) | | |
+| [Day 12](day12/) | | |
 
 ## How to Run
 
---
+++ day10/day10.cpp
@@ -0,0 +1,38 @@
+﻿#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::vector<string> read_input() {
+  vector<string> result;
+  ifstream input_file("input.txt");
+  string line;
+
+  while (getline(input_file, line)) {
+    result.push_back(line);
+  }
+
+  return result;
+}
+
+long long solve1(vector<string> input) {
+  return 0;
+}
+
+long long solve2(vector<string> input) {
+  return 0;
+}
+
+int main() {
+  std::vector<string> input = read_input();
+
+  cout << "Part 1: " << endl;
+  cout << solve1(input) << endl;
+  cout << "Part 2: " << endl;
+  cout << solve2(input) << endl;
+
+  return 0;
+}
