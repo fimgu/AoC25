ab3c3a0088571b029b5205a9a7da1fa9937ee03f
filipxg <filip.m.gunnarsson@gmail.com>
2025-12-02 10:11:47 +0100

    day2 cpp code O(1)

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -3,31 +3,70 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-std::vector<string> read_input() {
-  vector<string> result;
-  ifstream input_file("input.txt");
+std::vector<std::vector<long long>> read_input() {
+  vector<vector<long long>> result;
+  ifstream input_file("test.txt");
   string line;
+  long long a, b;
+  char hyphen;
+  char comma;
 
-  while (getline(input_file, line)) {
-    result.push_back(line);
+  while (input_file >> a >> hyphen >> b) {
+    result.push_back({a, b});
+    if (input_file.peek() == ',') {
+      input_file >> comma;
+    }
   }
 
   return result;
 }
 
-long long solve1(vector<string> input) {
-  return 0;
+bool is_halves_matching(long long n) {
+    int digits = (int)log10(n) + 1;
+    if (digits % 2 != 0) return false;
+    long long divisor = (long long)pow(10, digits / 2);
+    return (n / divisor) == (n % divisor);
 }
 
-long long solve2(vector<string> input) {
-  return 0;
+bool is_periodic(long long n) {
+    std::string s = std::to_string(n);
+    std::string doubled = s + s;
+    std::string trimmed = doubled.substr(1, doubled.length() - 2);
+    return trimmed.find(s) != std::string::npos;
+}
+
+long long solve1(vector<vector<long long>> input) {
+  long long res = 0;
+  for (auto &line : input) {
+    for (long long i = line[0]; i <= line[1]; ++i) {
+      if (is_halves_matching(i)) {
+        res += i;
+      }
+    }
+  }
+
+  return res;
+}
+
+long long solve2(vector<vector<long long>> input) {
+  long long res = 0;
+  for (auto &line : input) {
+    for (long long i = line[0]; i <= line[1]; ++i) {
+      if (is_periodic(i)) {
+        res += i;
+      }
+    }
+  }
+
+  return res;
 }
 
 int main() {
-  std::vector<string> input = read_input();
+  std::vector<vector<long long>> input = read_input();
 
   cout << "Part 1: " << endl;
   cout << solve1(input) << endl;
